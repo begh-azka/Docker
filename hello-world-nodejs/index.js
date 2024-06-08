c4c72a55d2699d681643e4f1cb8a48e7365cbc81
azka-aslam <97597065+begh-azka@users.noreply.github.com>
2024-06-08 16:02:28 +0530

    Update index.js

--- hello-world-nodejs/index.js
+++ hello-world-nodejs/index.js
@@ -2,14 +2,13 @@ var express = require('express')
 const process = require('process');
 
 var expressapp = express()
-expressapp.get('/', function (req, res) {
-    res.send('{"message":"Hello World JavaScript v1"}')
+expressapp.get('/', function(req, res) {
+    res.send("<p>Hello, World!</p>")
 })
-expressapp.listen(5000, function () {
-    console.log('Ready on port 5000!')
+expressapp.listen(5200, function () {
+    console.log('Ready on port 5200!')
 })
 
-/* To Handle Ctrl C*/
 process.on('SIGINT', function () {
     process.exit();
-});
+})
